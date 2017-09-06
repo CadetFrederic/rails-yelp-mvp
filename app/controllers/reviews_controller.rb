21f860ad21a918d60cc99219138782dc0650d531
Frederic Cadet <fc.cadet@gmail.com>
2017-09-06 18:12:47 +0200

    more or less done

---
+++ app/controllers/reviews_controller.rb
@@ -0,0 +1,34 @@
+class ReviewsController < ApplicationController
+
+  before_action :set_restaurant, only: [:index, :new, :create]
+
+  def index
+    @reviews = Review.where(restaurant_id: @restaurant.id)
+  end
+
+  def show
+  end
+
+  def new
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(review_params)
+    @review.restaurant_id = @restaurant.id
+    if @review.save
+      redirect_to restaurant_path(@restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+  def review_params
+    params.require(:review).permit(:content, :rating)
+  end
+
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,41 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes = [
+  {
+    name:         "Epicure au Bristol",
+    address:      "112 rue du Fg St-Honoré 75008 Paris",
+    phone_number:  "05 62 37 94 04",
+    category:     "french"
+  },
+  {
+    name:         "La truffière",
+    address:      "4 rue Blainville 75005 Paris",
+    phone_number:  "01 34 54 82 09",
+    category:      "belgian"
+  },
+  {
+    name:         "Le pré catelan",
+    address:      "route de Suresnes 75016 Paris",
+    phone_number:  "04 67 31 93 47",
+    category:     "chinese"
+  },
+    {
+    name:         "La tour d'argent",
+    address:      "rue du bac, 75008 Paris",
+    phone_number:  "01 21 56 98 41",
+    category:     "chinese"
+  },
+    {
+    name:         "Brasserie Montparnasse",
+    address:      "Boulevard Montparnasse 75015 Paris",
+    phone_number:  "01 ",
+    category:     "chinese"
+  }
+]
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
